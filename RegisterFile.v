8293ece4c3753cee628e718b760f38c048d8d4ed
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2025-07-03 22:24:06 +0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -4,21 +4,22 @@ module RegisterFile (
     input [4:0] addA, addB, addD,
     input [31:0] WB_out,
     input RegWrite,
-    output [31:0] dataA, dataB
+    output [31:0] dataA, dataB,
+    output [31:0] registers [0:31] // Thêm cổng đầu ra cho mảng registers
 );
-    reg [31:0] regs [0:31];
+    reg [31:0] registers [0:31]; // Đổi tên regs thành registers
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             for (int i = 0; i < 32; i++) begin
-                regs[i] <= 32'b0;
+                registers[i] <= 32'b0;
             end
         end
         else if (RegWrite && addD != 0) begin
-            regs[addD] <= WB_out;
+            registers[addD] <= WB_out;
         end
     end
 
-    assign dataA = regs[addA];
-    assign dataB = regs[addB];
+    assign dataA = registers[addA];
+    assign dataB = registers[addB];
 endmodule
